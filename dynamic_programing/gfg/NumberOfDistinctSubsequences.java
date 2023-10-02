e06b22318506f7bc377ed14664f6f760f2b2bdea
13413rahul <rahulk.ug19.cs@gmail.com>
2023-10-03 01:49:35 +0530

    coding

---
+++ dynamic_programing/gfg/NumberOfDistinctSubsequences.java
@@ -0,0 +1,30 @@
+package com.company.dynamic_programing.gfg;
+
+import java.util.HashMap;
+
+// https://practice.geeksforgeeks.org/problems/number-of-distinct-subsequences0909/1
+public class NumberOfDistinctSubsequences {
+    int distinctSubsequences(String s) {
+        // code here
+        long dp[] = new long[s.length()+1];
+        dp[0]=1;
+        int mod=(int)(1e9+7);
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=1;i<dp.length;i++)
+        {
+            dp[i]=(2*dp[i-1]%mod)%mod;
+            char ch=s.charAt(i-1);
+            if(hm.containsKey(ch))
+            {
+                int val=hm.get(ch);
+                dp[i]=(dp[i]%mod-dp[val-1]+mod)%mod;
+            }
+
+            hm.put(ch,i);
+
+        }
+
+        return (int)dp[s.length()]%mod;
+
+    }
+}
